cb3d56028c7112ee0e2c45cec626e27905590098
Naron <72578270+naronchen@users.noreply.github.com>
2022-02-20 04:47:26 +0000

    training in neural network via pytorch

---
+++ chatbot_train/nltk_utils.py
@@ -0,0 +1,34 @@
+#Train Data: bag of word -> put into Matrix -> Neuro Network
+#Tokenization + Steaming
+
+import nltk
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+
+
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype = np.float32)
+    for idx, w, in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
+    
+
+
+#test stem & tokenize
+# words = ["Organize", "organizes", "ogranizing"]
+# stemmed_words = [stem(w) for w in words]
+# print(stemmed_words)
+# a = "Aren't you tall?"
+# print(tokenize(a))
